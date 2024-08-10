cf4da1f2eced602efdb9f60f65bc5b99a9c0cabe
‘Mohamed <mohmed_bakry51@yahoo.com>
2024-08-10 14:18:18 +0200

    udpate the file

--- mumbleapi-master/discussion/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import (Discussion,DiscussionComment,DiscussionVote)
-
-
-admin.site.register(Discussion)
-admin.site.register(DiscussionComment)
-admin.site.register(DiscussionVote)
--- mumbleapi-master/feed/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Mumble, MumbleVote
-
-
-admin.site.register(Mumble)
-admin.site.register(MumbleVote)
